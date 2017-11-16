05ed0b0d9a0d77032551e92c49ccc6c0d4b0dcb6
CosmicEon <sillimanite90@gmail.com>
2017-11-16 12:46:13 +0200

    removed dist folder

--- api/constants/index.js
+++ api/constants/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DB_CONN_STRING: 'mongodb://localhost:27017/ts-the-heady-squids',
-  PORT: 8080,
+  DB_CONN_STRING: 'mongodb://test:test1234@ds111066.mlab.com:11066/ts-the-heady-squids', // mongodb://localhost:27017/ts-the-heady-squids
+  PORT: process.env.PORT || 8080,
 };
